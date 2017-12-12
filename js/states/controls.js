5504fd4c99d88bd8134d491d6506ade01a89e7b4
dpanayotov93 <dpanayotov93@gmail.com>
2017-12-12 17:20:41 +0200

    Updated Controls menu with Sprinting

--- js/states/controls.js
+++ js/states/controls.js
@@ -5,9 +5,10 @@ let stateControls = {
 		let movementLabel = game.add.bitmapText(settings.width / 2 - 320, 300, 'yggdrasil', 'Left/Right arrow keys: Move', 48);
 		let jumpLabel = game.add.bitmapText(settings.width / 2 - 220, 400, 'yggdrasil', 'UP arrow key: Jump', 48);
 		let attackLabel = game.add.bitmapText(settings.width / 2 - 170, 500, 'yggdrasil', 'Space: Attack', 48);
-		let skillLabel = game.add.bitmapText(settings.width / 2 - 180, 600, 'yggdrasil', 'LMB: Cast skill', 48);
-		let backButton = game.add.button(settings.width / 2 - settings.buttonSize.w / 2, 700, 'button', this.back, this, 0, 1, 2);		
-		let backLabel = game.add.bitmapText(settings.width / 2 - 60, 708, 'yggdrasil', 'Back', 46);
+		let sprintLabel = game.add.bitmapText(settings.width / 2 - 420, 600, 'yggdrasil', 'Shift + Left/Right arrow keys: Sprint', 48);
+		let skillLabel = game.add.bitmapText(settings.width / 2 - 180, 700, 'yggdrasil', 'LMB: Cast skill', 48);
+		let backButton = game.add.button(settings.width / 2 - settings.buttonSize.w / 2, 800, 'button', this.back, this, 0, 1, 2);		
+		let backLabel = game.add.bitmapText(settings.width / 2 - 60, 808, 'yggdrasil', 'Back', 46);
 
 		// Change the mouse when over a button
 		backButton.events.onInputOver.add(function(){
